699e0d3469b6b9b44fea24519c2c6324f524413c
shivam jadhav <jadhavshivam0228@gmail.com>
2020-10-04 17:26:37 +0530

    Updates

---
+++ precache-manifest.4612b55702a86c698ef859c15d2a5216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5443eb30e3dc347b1b8c242e8945a19c",
+    "url": "/FoodCartApp.git/index.html"
+  },
+  {
+    "revision": "a627196d7b3e82a75ef5",
+    "url": "/FoodCartApp.git/static/css/main.5a3d6a6c.chunk.css"
+  },
+  {
+    "revision": "b18ce50b4090fcb9c4d0",
+    "url": "/FoodCartApp.git/static/js/2.999d88f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FoodCartApp.git/static/js/2.999d88f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a627196d7b3e82a75ef5",
+    "url": "/FoodCartApp.git/static/js/main.ef5b2062.chunk.js"
+  },
+  {
+    "revision": "1ef1b6a72ea19e573dc9",
+    "url": "/FoodCartApp.git/static/js/runtime-main.58b68f27.js"
+  }
+]);
